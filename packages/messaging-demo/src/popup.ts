5e9bb217632993c8299189f1f32e9fc30efa77d7
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-11-19 11:14:01 -0600

    chore(messaging-demo): fix popup width and response formatting

--- packages/messaging-demo/src/popup.ts
+++ packages/messaging-demo/src/popup.ts
@@ -17,30 +17,30 @@ const [unknownButton, unknownPre] = getMessageElements('unknown');
 sleepButton.addEventListener('click', async () => {
   sleepPre.innerText = 'Loading...';
   const res = await sendMessage1('sleep', 1000).catch(err => ({ err: err.message }));
-  sleepPre.innerText = JSON.stringify(res);
+  sleepPre.innerText = JSON.stringify(res, null, 2);
 });
 
 pingButton.addEventListener('click', async () => {
   pingPre.innerText = 'Loading...';
   const res = await sendMessage1('ping', undefined).catch(err => ({ err: err.message }));
-  pingPre.innerText = JSON.stringify(res);
+  pingPre.innerText = JSON.stringify(res, null, 2);
 });
 
 ping2Button.addEventListener('click', async () => {
   ping2Pre.innerText = 'Loading...';
   const res = await sendMessage2('ping2', 'pong2').catch(err => ({ err: err.message }));
-  ping2Pre.innerText = JSON.stringify(res);
+  ping2Pre.innerText = JSON.stringify(res, null, 2);
 });
 
 throwButton.addEventListener('click', async () => {
   throwPre.innerText = 'Loading...';
   const res = await sendMessage2('throw', undefined).catch(err => ({ err: err.message }));
-  throwPre.innerText = JSON.stringify(res);
+  throwPre.innerText = JSON.stringify(res, null, 2);
 });
 
 unknownButton.addEventListener('click', async () => {
   unknownPre.innerText = 'Loading...';
   // @ts-expect-error: Testing what happens when an unknown key is passed in
   const res = await sendMessage2('unknown', undefined).catch(err => ({ err: err.message }));
-  unknownPre.innerText = JSON.stringify(res);
+  unknownPre.innerText = JSON.stringify(res, null, 2);
 });
