a2057243d20798fcbacdfe7c9424ee5ebe9df097
Aaron <aklinker1@users.noreply.github.com>
2023-02-03 19:11:32 -0600

    feat(storage): Add `onChange` listeners (#10)

--- packages/storage/src/types.ts
+++ packages/storage/src/types.ts
@@ -1,5 +1,15 @@
 export type AnySchema = Record<string, any>;
 
+/**
+ * Call this method to remove the listener that was added.
+ */
+export type RemoveListenerCallback = () => void;
+
+export type OnChangeCallback<
+  TSchema extends AnySchema,
+  TKey extends keyof TSchema = keyof TSchema,
+> = (newValue: TSchema[TKey], oldValue: TSchema[TKey] | null) => void;
+
 export interface ExtensionStorage<TSchema extends AnySchema> {
   /**
    * Clear all values from storage.
@@ -21,4 +31,15 @@ export interface ExtensionStorage<TSchema extends AnySchema> {
    * Remove the value from storage at a key.
    */
   removeItem<TKey extends keyof TSchema>(key: TKey): Promise<void>;
+
+  /**
+   * Add a callback that is executed when a key is changed in the storage. Listeners are executed in
+   * parallel, but the first listeners added are always started first.
+   *
+   * Returns a method that, when called, removes the listener that was added.
+   */
+  onChange<TKey extends keyof TSchema>(
+    key: TKey,
+    cb: OnChangeCallback<TSchema, TKey>,
+  ): RemoveListenerCallback;
 }
