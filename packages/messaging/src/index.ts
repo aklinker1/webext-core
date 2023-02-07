50aaf28606e4dbde822fb9301816d005a934eacc
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-02-07 11:09:11 -0600

    docs(messenger): Improve inline documentation

--- packages/messaging/src/index.ts
+++ packages/messaging/src/index.ts
@@ -1,56 +1,154 @@
 import Browser, { Runtime } from 'webextension-polyfill';
 
-export interface ExtensionMessagingConfig {
-  logger?: {
-    debug(...args: any[]): void;
-    log(...args: any[]): void;
-    warn(...args: any[]): void;
-    error(...args: any[]): void;
-  };
+/**
+ * Interface used to log text to the console when sending and recieving messages.
+ */
+export interface Logger {
+  debug(...args: any[]): void;
+  log(...args: any[]): void;
+  warn(...args: any[]): void;
+  error(...args: any[]): void;
 }
 
-// Use randomized keys so they don't conflict with payload-only types
-export type ProtocolWithReturn<TData, TResponse> = { BtVgCTPYZu: TData; RrhVseLgZW: TResponse };
+export interface ExtensionMessagingConfig {
+  /**
+   * The logger to use when logging messages. Defaults to `console`.
+   *
+   * Set to `null` to disable logging.
+   */
+  logger?: Logger;
+}
 
-type GetData<T> = T extends ProtocolWithReturn<any, any> ? T['BtVgCTPYZu'] : T;
-type GetResponse<T> = T extends ProtocolWithReturn<any, any> ? T['RrhVseLgZW'] : void;
+/**
+ * Used to add a return type to a message in the protocol map.
+ *
+ * @example
+ * interface ProtocolMap {
+ *   // data is a string, returns undefined
+ *   type1: string;
+ *   // data is a string, returns a number
+ *   type2: ProtocolWithReturn<string, number>;
+ * }
+ *
+ * > Internally, this is just an object with random keys for the data and return types.
+ */
+export type ProtocolWithReturn<TData, TReturn> = { BtVgCTPYZu: TData; RrhVseLgZW: TReturn };
 
-interface Message<TProtocolMap, TKey extends keyof TProtocolMap> {
+/**
+ * Given a `ProtocolWithReturn` or a value, return the message's data type.
+ */
+export type GetDataType<T> = T extends ProtocolWithReturn<any, any> ? T['BtVgCTPYZu'] : T;
+
+/**
+ * Given a `ProtocolWithReturn` or a value, return the message's return type.
+ */
+export type GetReturnType<T> = T extends ProtocolWithReturn<any, any> ? T['RrhVseLgZW'] : void;
+
+/**
+ * Either a Promise of a type, or that type directly. Used to indicate that a method can by sync or
+ * async.
+ */
+export type MaybePromise<T> = Promise<T> | T;
+
+/**
+ * Call to ensure an active listener has been removed.
+ *
+ * If the listener has already been removed with `Messenger.removeAllListeners`, this is a noop.
+ */
+export type RemoveListenerCallback = () => void;
+type OnMessageReceived<TProtocolMap, TType extends keyof TProtocolMap> = (
+  message: Message<TProtocolMap, TType>,
+) => MaybePromise<GetReturnType<TProtocolMap[TType]>>;
+
+/**
+ * Use the functions defined in the messenger to send and recieve messages throughout your entire extension.
+ *
+ * Unlike the regular `chrome.runtime` messaging APIs, there are no limitations to when you call `onMessage` or `sendMessage`.
+ */
+export interface Messenger<TProtocolMap> {
+  /**
+   * Send a message to the background or a specific tab if `tabId` is passed. You can call this
+   * function anywhere in your extension.
+   *
+   * If you haven't setup a listener for the sent `type`, an error will be thrown.
+   *
+   * @param type The message type being sent. Call `onMessage` with the same type to listen for that message.
+   * @param data The data to send with the message.
+   * @param tabId An optional ID specifying which tab the message should be sent to.
+   */
+  sendMessage<TType extends keyof TProtocolMap>(
+    type: TType,
+    data: GetDataType<TProtocolMap[TType]>,
+    tabId?: number,
+  ): Promise<GetReturnType<TProtocolMap[TType]>>;
+
+  /**
+   * Trigger a callback when a message of the requested type is recieved.
+   *
+   * To remove the listener, call the returned message.
+   *
+   * @param type The message type to listen for. Call `sendMessage` with the same type to triggern this listener.
+   * @param onReceived The callback executed when a message is recieved.
+   */
+  onMessage<TType extends keyof TProtocolMap>(
+    type: TType,
+    onReceived: OnMessageReceived<TProtocolMap, TType>,
+  ): RemoveListenerCallback;
+}
+
+/**
+ * Contains information about the message recieved.
+ */
+interface Message<TProtocolMap, TType extends keyof TProtocolMap> {
+  /**
+   * A semi-unique, auto-incrementing number used to trace messages being sent.
+   */
   id: number;
-  data: GetData<TProtocolMap[TKey]>;
+  /**
+   * The data that was passed into `sendMessage`
+   */
+  data: GetDataType<TProtocolMap[TType]>;
+  /**
+   * See [`MessageSender`](https://developer.chrome.com/docs/extensions/reference/runtime/#type-MessageSender).
+   * This contains information about which JS context sent the message.
+   */
   sender: Runtime.MessageSender;
+  /**
+   * The timestamp the message was sent in MS since epoch.
+   */
   timestamp: number;
 }
 
-type MaybePromise<T> = Promise<T> | T;
-
-type OnMessageReceived<TProtocolMap, TKey extends keyof TProtocolMap> = (
-  message: Message<TProtocolMap, TKey>,
-) => MaybePromise<GetResponse<TProtocolMap[TKey]>>;
-
-export function defineExtensionMessaging<TProtocolMap>(config?: ExtensionMessagingConfig) {
+/**
+ * Creates a new `Messenger` used to send and recieve messages in your extension.
+ *
+ * @param config Configure the behavior of the messenger.
+ */
+export function defineExtensionMessaging<TProtocolMap>(
+  config?: ExtensionMessagingConfig,
+): Messenger<TProtocolMap> {
   let rootListener:
     | undefined
     | ((message: any, sender: Browser.Runtime.MessageSender) => void | Promise<any>);
-  let keyListeners: { [key in keyof TProtocolMap]?: Function } = {};
+  let keyListeners: { [type in keyof TProtocolMap]?: Function } = {};
 
   let idSeq = Math.floor(Math.random() * 10000);
   function getNextId(): number {
     return idSeq++;
   }
 
-  function onMessage<TKey extends keyof TProtocolMap>(
-    key: TKey,
-    onReceived: OnMessageReceived<TProtocolMap, TKey>,
+  function onMessage<TType extends keyof TProtocolMap>(
+    type: TType,
+    onReceived: OnMessageReceived<TProtocolMap, TType>,
   ) {
     if (rootListener == null) {
       config?.logger?.debug(
-        `[messaging] ${key as string} initialized message listener for this context`,
+        `[messaging] ${type as string} initialized message listener for this context`,
       );
       rootListener = (message, sender) => {
-        if (typeof message.key != 'string' || typeof message.timestamp !== 'number') {
+        if (typeof message.type != 'string' || typeof message.timestamp !== 'number') {
           const err = Error(
-            `[messaging] Unknown message format, must include the 'key' & 'timestamp' fields, received: ${JSON.stringify(
+            `[messaging] Unknown message format, must include the 'type' & 'timestamp' fields, received: ${JSON.stringify(
               message,
             )}`,
           );
@@ -60,8 +158,8 @@ export function defineExtensionMessaging<TProtocolMap>(config?: ExtensionMessagi
 
         config?.logger?.debug('[messaging] Received message', message, sender);
         const entries = Object.entries(keyListeners) as Array<[keyof TProtocolMap, Function]>;
-        for (const [key, listener] of entries) {
-          if (message.key !== key) continue;
+        for (const [type, listener] of entries) {
+          if (message.type !== type) continue;
           config?.logger?.debug(`[messaging] onMessage {id=${message.id}} ᐊ─`, message);
 
           const res = listener({
@@ -85,24 +183,24 @@ export function defineExtensionMessaging<TProtocolMap>(config?: ExtensionMessagi
         }
 
         return Promise.resolve({
-          err: `Listener not found for message.type="${message.key}".\n\nDid you forget to call \`onMessage("getLength", ...)\`?`,
+          err: `Listener not found for message.type="${message.type}".\n\nDid you forget to call \`onMessage("getLength", ...)\`?`,
         });
       };
       Browser.runtime.onMessage.addListener(rootListener);
     }
 
-    if (keyListeners[key] != null) {
+    if (keyListeners[type] != null) {
       const err = Error(
-        `[messaging] In this JS context, only one listener can be setup for ${key as string}`,
+        `[messaging] In this JS context, only one listener can be setup for ${type as string}`,
       );
       config?.logger?.error(err);
       throw err;
     }
-    keyListeners[key] = onReceived;
-    config?.logger?.log(`[messaging] Added listener for ${key as string}`);
+    keyListeners[type] = onReceived;
+    config?.logger?.log(`[messaging] Added listener for ${type as string}`);
 
     return function removeListener(): void {
-      delete keyListeners[key];
+      delete keyListeners[type];
       if (Object.entries(keyListeners).length === 0) {
         // @ts-expect-error: rootListener's type is not what is expected
         Browser.runtime.onMessage.removeListener(rootListener);
@@ -111,12 +209,12 @@ export function defineExtensionMessaging<TProtocolMap>(config?: ExtensionMessagi
     };
   }
 
-  async function sendMessage<TKey extends keyof TProtocolMap>(
-    key: TKey,
-    data: GetData<TProtocolMap[TKey]>,
+  async function sendMessage<TType extends keyof TProtocolMap>(
+    type: TType,
+    data: GetDataType<TProtocolMap[TType]>,
     tabId?: number,
-  ): Promise<GetResponse<TProtocolMap[TKey]>> {
-    const message = { id: getNextId(), key, data, timestamp: Date.now() };
+  ): Promise<GetReturnType<TProtocolMap[TType]>> {
+    const message = { id: getNextId(), type, data, timestamp: Date.now() };
     config?.logger?.debug(`[messaging] sendMessage {id=${message.id}} ─ᐅ`, message);
 
     let response;
