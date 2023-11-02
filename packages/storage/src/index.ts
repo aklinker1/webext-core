49ebe5a432971daaa99b671c47ef5ce14c9ddbc6
Aaron <aaronklinker1@gmail.com>
2023-11-02 08:32:02 -0500

    feat(storage): Add `sessionExtStorage` (#47)

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -9,6 +9,14 @@ export { defineExtensionStorage };
  */
 export const localExtStorage = defineExtensionStorage(Browser.storage.local);
 /**
+ * An implementation of `ExtensionStorage` based on the `browser.storage.local` storage area.
+ *
+ * - Added to Chrome 102 as of May 24th, 2022.
+ * - Added to Safari 16.4 as of March 27th, 2023.
+ * - Added to Firefox 115 as of July 4th, 2023.
+ */
+export const sessionExtStorage = defineExtensionStorage(Browser.storage.session);
+/**
  * An implementation of `ExtensionStorage` based on the `browser.storage.sync` storage area.
  */
 export const syncExtStorage = defineExtensionStorage(Browser.storage.sync);
