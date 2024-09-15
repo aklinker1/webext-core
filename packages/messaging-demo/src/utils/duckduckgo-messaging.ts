e6fe4a672a1ad9627294be160e4526f8a54d3665
1natsu <1natsu172@users.noreply.github.com>
2024-09-15 23:57:10 +0900

    chore

--- packages/messaging-demo/src/utils/duckduckgo-messaging.ts
+++ packages/messaging-demo/src/utils/duckduckgo-messaging.ts
@@ -3,10 +3,8 @@ import { defineCustomEventMessaging } from '@webext-core/messaging/page';
 export interface DuckduckgoMessagingProtocol {
   ping(): string;
   ping2(): string;
-  ping3(): string;
   fromInjected(): string;
   fromInjected2(): string;
-  fromInjected3(): string;
 }
 
 export const duckduckgoMessaging = defineCustomEventMessaging<DuckduckgoMessagingProtocol>({
--- packages/messaging-demo/src/utils/google-messaging.ts
+++ packages/messaging-demo/src/utils/google-messaging.ts
@@ -3,10 +3,8 @@ import { defineWindowMessaging } from '@webext-core/messaging/page';
 export interface GoogleMessagingProtocol {
   ping(): string;
   ping2(): string;
-  ping3(): string;
   fromInjected(): string;
   fromInjected2(): string;
-  fromInjected3(): string;
 }
 
 export const googleMessaging = defineWindowMessaging<GoogleMessagingProtocol>({
