5b2b4bd2d5caa865f2e51419d990cf736f94338e
Aaron <aaronklinker1@gmail.com>
2026-01-03 12:22:03 -0600

    fix demo

--- packages/proxy-service-demo/src/entrypoints/popup/main.ts
+++ packages/proxy-service-demo/src/entrypoints/popup/main.ts
@@ -1,3 +1,5 @@
+import { createProxyService } from '@webext-core/proxy-service';
+
 function getMathServiceElements(id: string) {
   const parent = document.getElementById(id)!;
   return [
@@ -12,34 +14,34 @@ const [multiplyButton, multiplyPre] = getMathServiceElements('multiply');
 const [divideButton, dividePre] = getMathServiceElements('divide');
 const [factorialButton, factorialPre] = getMathServiceElements('factorial');
 
-const MathService = getMathService();
+const mathService = createProxyService(MathServiceKey);
 
 addButton.addEventListener('click', async () => {
   addPre.innerText = 'Loading...';
-  const res = await MathService.add(1, 2).catch(err => ({ err: err.message }));
+  const res = await mathService.add(1, 2).catch(err => ({ err: err.message }));
   addPre.innerText = JSON.stringify(res, null, 2);
 });
 
 subtractButton.addEventListener('click', async () => {
   subtractPre.innerText = 'Loading...';
-  const res = await MathService.subtract(2, 1).catch(err => ({ err: err.message }));
+  const res = await mathService.subtract(2, 1).catch(err => ({ err: err.message }));
   subtractPre.innerText = JSON.stringify(res, null, 2);
 });
 
 multiplyButton.addEventListener('click', async () => {
   multiplyPre.innerText = 'Loading...';
-  const res = await MathService.multiply(2, 3).catch(err => ({ err: err.message }));
+  const res = await mathService.multiply(2, 3).catch(err => ({ err: err.message }));
   multiplyPre.innerText = JSON.stringify(res, null, 2);
 });
 
 divideButton.addEventListener('click', async () => {
   dividePre.innerText = 'Loading...';
-  const res = await MathService.divide(1, 0).catch(err => ({ err: err.message }));
+  const res = await mathService.divide(1, 0).catch(err => ({ err: err.message }));
   dividePre.innerText = JSON.stringify(res, null, 2);
 });
 
 factorialButton.addEventListener('click', async () => {
   factorialPre.innerText = 'Loading...';
-  const res = await MathService.factorial(100).catch(err => ({ err: err.message }));
+  const res = await mathService.factorial(100).catch(err => ({ err: err.message }));
   factorialPre.innerText = JSON.stringify(res, null, 2);
 });
---
+++ packages/proxy-service-demo/src/utils/proxy-service-keys.ts
@@ -0,0 +1,4 @@
+import { ProxyServiceKey } from '@webext-core/proxy-service';
+import type { MathService } from './math-service';
+
+export const MathServiceKey = 'math-service' as ProxyServiceKey<MathService>;
