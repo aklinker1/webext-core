04414915d7e6604cb4dc7cfe4c684277d8f15102
Aaron <aaronklinker1@gmail.com>
2026-07-24 22:17:42 -0500

    chore!: Drop CJS support

--- packages/messaging/tsdown.config.ts
+++ packages/messaging/tsdown.config.ts
@@ -6,7 +6,7 @@ export default defineConfig([
   {
     outDir: 'lib',
     entry: ['src/index.ts', 'src/page.ts'],
-    format: ['esm', 'cjs'],
+    format: 'esm',
   },
   {
     outDir: 'lib',
--- packages/proxy-service/tsdown.config.ts
+++ packages/proxy-service/tsdown.config.ts
@@ -6,7 +6,7 @@ export default defineConfig([
   {
     outDir: 'lib',
     entry: ['src/index.ts', 'src/types.ts'],
-    format: ['esm', 'cjs'],
+    format: 'esm',
   },
   {
     outDir: 'lib',
